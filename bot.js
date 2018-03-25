4c2af849b6628c5a2f5e45e8d22efc62077bfb51
alRebel711 <37731644+alRebel711@users.noreply.github.com>
2018-03-25 16:01:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,17 +220,57 @@ ${guild.memberCount} أعضآء السيرفر
 `)
 });
 
+clinet.on('message', message => {
+if (message.content.startsWith(prefix + "uptime")) {
+    let uptime = client.uptime;
+
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+
+    while (notCompleted) {
+
+        if (uptime >= 8.64e+7) {
+
+            days++;
+            uptime -= 8.64e+7;
+
+        } else if (uptime >= 3.6e+6) {
+
+            hours++;
+            uptime -= 3.6e+6;
+
+        } else if (uptime >= 60000) {
+
+            minutes++;
+            uptime -= 60000;
+
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+
+        }
+
+        if (uptime < 1000)  notCompleted = false;
+
+    }
+
+    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
 
 
-
-client.on('guildCreate', guild => {
-    
-  var rebel = new Discord.RichEmbed()
-  .setColor(000000)
-            .addField(`R-Bot .`, 'شكرآ لتعآملك مع بوتنآ وإدخآله سيرفرك');
-      guild.owner.send(rebel);
+}
 });
 
+client.on('guildCreate', guild => {
+  var embed = new Discord.RichEmbed()
+  .setColor(0x5500ff)
+  .setThumbnail(guild.author.avatarURL)
+  .setDescription(`**شكراً لك لإضافه البوت الى سيرفرك **
+                              "R-Bot"   ونشكر تعآملك مع آر بوت `)
+      guild.owner.send(embed)
+});
 
 
 
